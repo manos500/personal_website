c481ac2edc935eadc307127c5d449db2bef51269
Manos Nikitakis <manosnikitakis3@gmail.com>
2022-09-26 13:42:24 +0300

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { navLinks } from "../constants";
+import {logo,logo2,close,menu} from '../assets';
+import { useState } from "react";
+
+const Navbar = () => {
+ const[toggle,setToggle] = useState(false)
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar ">
+      <img src={logo} alt="logo" className="w-[110px] h-[100px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1 ">
+        {navLinks.map((nav,index) => (<li key={nav.id} className={`font-poppins cursor-pointer font-normal text-[16px] text-white  hover:opacity-50  ${index === navLinks.length-1 ? "mr-0" : "mr-10"} ${toggle ? "sm:hidden" : "flex"}`}>
+          <a href={`#${nav.id}`}>{nav.title}</a>
+        </li>))}
+      </ul>
+
+      <div className={` justify-end items-center ${toggle ? 'flex' :'sm:hidden'} `}>
+        <img src={menu} alt="menu"  className={`${toggle ? 'hidden' : 'flex'} w-[28px] h-[28px] object-contain cursor-pointer`} onClick={() => setToggle((prev) => !prev)}/>
+         <div className={`${toggle ? 'flex' : 'hidden'} p-6  bg-black-gradient absolute top-0 right-0  min-w-[200px]  sidebar flex-col`}>
+          <div className=" w-[200px] justify-end items-end" >
+            <img src={close} alt="close" className={`${toggle ? 'flex' : 'hidden'} w-[28px] h-[28px] object-contain cursor-pointer  hover:opacity-50`} onClick={() => setToggle((prev) => !prev)}/>
+          </div> 
+           <ul className='list-none flex flex-col justify-start my-12 items-start flex-1'>
+          {navLinks.map((nav,index) => (<li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px]  hover:opacity-50  ${index === navLinks.length-1 ? 'mr-0' : 'mb-4'} text-white`}>
+          <a href={`#${nav.id}`}>{nav.title}</a>
+        </li>))}
+      </ul>
+         </div>
+      </div>
+     
+    </nav>
+  )
+}
+
+export default Navbar
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,15 +1,43 @@
-import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+import { github, instagram, youtube, discord} from "../assets";
 
-export const white_pawns = [
+export const navLinks = [
   {
-    id:"white_pawns",
-    img: people01,
-  }
-]
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
 
-export const black_pawns = [
+export const socialMedia = [
   {
-    id:"black_pawns",
-    img: people02,
-  }
-]
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: github,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: youtube,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: discord,
+    link: "https://www.linkedin.com/",
+  },
+];
