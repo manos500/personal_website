b1d0ba9a550c0bb2d82bea8b9896d0cabd6c7619
Manos Nikitakis <manosnikitakis3@gmail.com>
2022-09-26 11:10:33 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import FeedBackCard from "./FeedBackCard"
+import Business from "./Business";
+import Clients from "./Clients";
+import Button from "./Button";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+  Button,
+  FeedBackCard,
+  GetStarted,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,15 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const white_pawns = [
+  {
+    id:"white_pawns",
+    img: people01,
+  }
+]
+
+export const black_pawns = [
+  {
+    id:"black_pawns",
+    img: people02,
+  }
+]
