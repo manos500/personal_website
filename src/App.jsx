4cfb83a44ca37ef9c3d9681c11aed30c00047798
Manos Nikitakis <manosnikitakis3@gmail.com>
2022-11-01 13:45:59 +0200

    added clock

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,36 @@ import {
   Career, Navbar, Hero, Footer, Contact,Projects
 } from './components';
 
+import { useState } from "react";
+
 import styles from './style';
 
-function App() {
+function App() {  
+
+  let time  = new Date().toLocaleTimeString()
+
+  const [ctime,setTime] = useState(time)
+  const UpdateTime=()=>{
+    time =  new Date().toLocaleTimeString()
+    setTime(time)
+  }
+  setInterval(UpdateTime)
+  
   return (
 
     <div className="bg-primary w-full overflow-hidden ">
       <div className={`${styles.paddingX} ${styles.flexCenter}   bg-gray-gradient w-full fixed_position box_shadow rounded-lg `}>
+        
 
         <div className={`${styles.boxWidth} `}>
           <Navbar />
         </div>
       </div>
+     
 
-      <div className={`bg-primary ${styles.flexStart} mt-20 sm:mb-40 min-h-[600px]`} id="home">
-        <div className={`${styles.boxWidth} `}>
+      <div className={`bg-primary ${styles.flexStart} mt-20 sm:mb-40 min-h-[600px]`} id="home">      
+       <h1 className="clock_heading">{ctime}</h1>  
+        <div className={`${styles.boxWidth} `}>          
           <Hero />
         </div>
       </div>
